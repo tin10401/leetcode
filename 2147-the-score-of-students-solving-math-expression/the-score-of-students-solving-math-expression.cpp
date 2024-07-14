5c06b832a34578611dc600dc002c215d801300d6
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-14 14:52:23 -0700

    Time: 426 ms (97.10%) | Memory: 171.9 MB (89.85%) - LeetSync

--- 2147-the-score-of-students-solving-math-expression/the-score-of-students-solving-math-expression.cpp
+++ 2147-the-score-of-students-solving-math-expression/the-score-of-students-solving-math-expression.cpp
@@ -1,35 +1,80 @@
 class Solution {
 public:
-    unordered_set<int> memo[32][32];
-void dfs(string &s, int st, int fin) {
-    if (memo[st][fin].empty()) {
-        if (fin - st == 1)
-            memo[st][fin].insert(s[st] - '0');
-        for (int i = st + 1; i < fin; i += 2) {
-                dfs(s, st, i);
-                dfs(s, i + 1, fin);
-                for (int n1 : memo[st][i]) {
-                    for (int n2 : memo[i + 1][fin]) {
-                            int n = s[i] == '*' ? n1 * n2 : n1 + n2;
-                            if (n <= 1000)
-                                memo[st][fin].insert(n);
-                     }
+    // unordered_map<string_view, vector<int>> dp;
+    unordered_set<int> dp[32][32];
+    void dfs(const string& s, int leftPart, int rightPart)
+    {
+        // auto curr = string_view(s).substr(leftPart, rightPart - leftPart + 1);
+        auto& res = dp[leftPart][rightPart];
+        if(!res.empty()) return;
+        for(int i = leftPart; i < rightPart; i++)
+        {
+            char ch = s[i];
+            if(!isdigit(ch))
+            {
+                dfs(s, leftPart, i);
+                dfs(s, i + 1, rightPart);
+                for(auto& l : dp[leftPart][i])
+                {
+                    for(auto& r : dp[i + 1][rightPart])
+                    {
+                        int ans = ch == '+' ? l + r : l * r;
+                        if(ans <= 1000) res.insert(ans);
+                    }
                 }
+            }
         }
+        if(res.empty()) res.insert(stoi(s.substr(leftPart, rightPart - leftPart + 1)));
     }
-}
-int scoreOfStudents(string s, vector<int>& answers) {
-    int correct = 0;
-    for (int i = 1, j = 0; i <= s.size(); i += 2) 
-        if (i == s.size() || s[i] == '+') {
-            int mul = 1;
-            for (; j < i; j += 2)
-                mul *= s[j] - '0';
-            correct += mul;
+    int scoreOfStudents(string s, vector<int>& answers) {
+        int n = s.size();
+        using ll = long long;
+        stack<int> st;
+        int curr = 0;
+        char last = '+';
+        for(auto& ch : s)
+        {
+            if(isdigit(ch))
+            {
+                curr = curr * 10 + (ch - '0');
+            }
+            else
+            {
+                if(last == '+') st.push(curr);
+                else 
+                {
+                    auto num = st.top();
+                    st.pop();
+                    st.push(num * curr);
+                }
+                curr = 0;
+                last = ch;
+            }
         }
-    dfs(s, 0, s.size());
-    return accumulate(begin(answers), end(answers), 0, [&](int sum, int ans){
-        return sum + (ans == correct ? 5 :  memo[0][s.size()].count(ans) ? 2 : 0);
-    });
-}
+        int correct = 0;
+        if(last == '*') 
+        {
+            auto num = st.top();
+            st.pop();
+            correct = num * curr;
+        }
+        else
+        {
+            correct = curr;
+        }
+        while(!st.empty())
+        {
+            correct += st.top();
+            st.pop();
+        }
+        dfs(s, 0, s.size());
+        unordered_set<int> &partiallyCorrect = dp[0][n];
+        int res = 0;
+        for(auto& num : answers)
+        {
+            if(num == correct) res += 5;
+            else if(partiallyCorrect.count(num)) res += 2;
+        }
+        return res;
+    }
 };
