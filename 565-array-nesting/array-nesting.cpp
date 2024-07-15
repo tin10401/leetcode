2b03e8c53e4247d9c3914ee1081d162f18387af1
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-15 12:59:17 -0700

    Time: 97 ms (75.06%) | Memory: 103.1 MB (33.41%) - LeetSync

--- 565-array-nesting/array-nesting.cpp
+++ 565-array-nesting/array-nesting.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    unordered_set<int> vis;
+    vector<int> vis;
     int dfs(int node)
     {
-        if(vis.count(node)) return 0;
-        vis.insert(node);
+        if(vis[node]) return 0;
+        vis[node] = true;
         return 1 + dfs(nums[node]);
     }
     vector<int> nums;
     int arrayNesting(vector<int>& nums) {
         this->nums = nums;
+        vis.resize(nums.size());
         int res = 0;
         for(int i = 0; i < nums.size(); i++)
         {
