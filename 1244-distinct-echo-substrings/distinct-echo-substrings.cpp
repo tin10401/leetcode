bd5571fcdff964a8327d47151f781310bf671cd9
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-16 08:14:09 -0700

    Time: 301 ms (45.41%) | Memory: 9.6 MB (68.37%) - LeetSync

---
+++ 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int distinctEchoSubstrings(string s) {
+        int mod = 1e9 + 7;
+        unordered_set<string_view> vis;
+        int n = s.size();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; i - j >= 0 && i + j + 1 < n; j++)
+            {
+                auto left = string_view(s).substr(i - j, j + 1);
+                auto right = string_view(s).substr(i + 1, j + 1);
+                if(left == right) vis.insert(left);
+            }
+            cout << vis.size() << endl;
+        }
+        return vis.size();
+    }
+};
