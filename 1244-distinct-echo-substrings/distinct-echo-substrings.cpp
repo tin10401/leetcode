a3a92b55c0fd09a198687ac6604fb03c63004766
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-16 08:29:53 -0700

    Time: 111 ms (75.51%) | Memory: 9.3 MB (78.57%) - LeetSync

--- 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
+++ 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
@@ -1,16 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int distinctEchoSubstrings(string s) {
-        int mod = 1e9 + 7;
-        unordered_set<string_view> vis;
+        using ll = long long;
+        ll mod1 = 1e9 + 7, mod2 = 1e9 + 33;
         int n = s.size();
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; i - j >= 0 && i + j + 1 < n; j++)
-            {
-                auto left = string_view(s).substr(i - j, j + 1);
-                auto right = string_view(s).substr(i + 1, j + 1);
-                if(left == right) vis.insert(left);
+        vector<ll> hash1(n + 1, 0), hash2(n + 1, 0);
+        vector<ll> pow1(n + 1, 1), pow2(n + 1, 1);
+        
+        for (int i = 1; i <= n; ++i) {
+            hash1[i] = (hash1[i - 1] * 26 + (s[i - 1] - 'a' + 1)) % mod1;
+            hash2[i] = (hash2[i - 1] * 26 + (s[i - 1] - 'a' + 1)) % mod2;
+            pow1[i] = (pow1[i - 1] * 26) % mod1;
+            pow2[i] = (pow2[i - 1] * 26) % mod2;
+        }
+
+        set<pair<ll, ll>> vis;
+        for (int len = 1; len * 2 <= n; ++len) {
+            for (int i = 0; i + 2 * len <= n; ++i) {
+                ll leftHash1 = (hash1[i + len] - hash1[i] * pow1[len] % mod1 + mod1) % mod1;
+                ll leftHash2 = (hash2[i + len] - hash2[i] * pow2[len] % mod2 + mod2) % mod2;
+                ll rightHash1 = (hash1[i + 2 * len] - hash1[i + len] * pow1[len] % mod1 + mod1) % mod1;
+                ll rightHash2 = (hash2[i + 2 * len] - hash2[i + len] * pow2[len] % mod2 + mod2) % mod2;
+                if (leftHash1 == rightHash1 && leftHash2 == rightHash2) {
+                    vis.insert({leftHash1, leftHash2});
+                }
             }
         }
         return vis.size();
