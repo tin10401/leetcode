1950deb6f24aa1a02608ec982901cf9538904588
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-16 10:48:29 -0700

    Time: 544 ms (72.45%) | Memory: 221 MB (70.41%) - LeetSync

--- 2068-maximum-genetic-difference-query/maximum-genetic-difference-query.cpp
+++ 2068-maximum-genetic-difference-query/maximum-genetic-difference-query.cpp
@@ -1,94 +1,83 @@
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-
-using namespace std;
-
-class Trie {
-public:
+class Trie
+{
+    public:
     Trie* children[2];
     int count;
+    Trie() : count(0), children{} {}
 
-    Trie() : children{nullptr, nullptr}, count(0) {}
-
-    void insert(int num) {
+    void insert(int num)
+    {
         Trie* curr = this;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >> i) & 1;
-            if (!curr->children[bit]) {
-                curr->children[bit] = new Trie();
-            }
-            curr = curr->children[bit];
+        for(int i = 31; i >= 0; i--)
+        {
+            int bits = (num >> i) & 1;
+            if(!curr->children[bits]) curr->children[bits] = new Trie();
+            curr = curr->children[bits];
             curr->count++;
         }
     }
 
-    void erase(int num) {
+    void erase(int num)
+    {
         Trie* curr = this;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >> i) & 1;
-            curr = curr->children[bit];
+        for(int i = 31; i >= 0; i--)
+        {
+            int bits = (num >> i) & 1;
+            curr = curr->children[bits];
             curr->count--;
         }
     }
 
-    int search(int num) {
+    int search(int num)
+    {
         Trie* curr = this;
         int res = 0;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >> i) & 1;
-            if (curr->children[!bit] && curr->children[!bit]->count > 0) {
+        for(int i = 31; i >= 0; i--)
+        {
+            int bits = (num >> i) & 1;
+            if(curr->children[!bits] && curr->children[!bits]->count)
+            {
+                curr = curr->children[!bits];
                 res |= (1 << i);
-                curr = curr->children[!bit];
-            } else {
-                curr = curr->children[bit];
             }
+            else curr = curr->children[bits];
         }
         return res;
     }
 };
-
 class Solution {
 public:
-    vector<vector<int>> graph;
-    vector<vector<pair<int, int>>> nodeQueries;
-    vector<int> res;
-    Trie trie;
-
-    void dfs(int node) {
-        trie.insert(node);
-        for (const auto& [queryIndex, val] : nodeQueries[node]) {
-            res[queryIndex] = trie.search(val);
+    void dfs(int node)
+    {
+        trie->insert(node);
+        for(auto& [val, index] : nodes[node])
+        {
+            res[index] = trie->search(val);
         }
-        for (int child : graph[node]) {
-            dfs(child);
+        for(auto& nei : graph[node])
+        {
+            dfs(nei);
         }
-        trie.erase(node);
+        trie->erase(node);
     }
-
+    vector<vector<int>> graph;
+    vector<vector<pair<int, int>>> nodes;
+    vector<int> res;
+    Trie* trie = new Trie();
     vector<int> maxGeneticDifference(vector<int>& parents, vector<vector<int>>& queries) {
-        int n = parents.size();
-        graph.resize(n);
-        nodeQueries.resize(n);
-        res.resize(queries.size());
-
-        int root = -1;
-        for (int i = 0; i < n; ++i) {
-            if (parents[i] == -1) {
-                root = i;
-            } else {
-                graph[parents[i]].push_back(i);
-            }
+        int startNode = -1, n = parents.size();
+        graph.resize(n), res.resize(queries.size()), nodes.resize(n);
+        for(int i = 0; i < n; i++)
+        {
+            if(parents[i] == -1) startNode = i;
+            else graph[parents[i]].push_back(i);
         }
-
-        for (int i = 0; i < queries.size(); ++i) {
-            int node = queries[i][0];
-            int val = queries[i][1];
-            nodeQueries[node].emplace_back(i, val);
+        for(int i = 0; i < queries.size(); i++)
+        {
+            int node = queries[i][0], val = queries[i][1];
+            nodes[node].push_back({val, i});
         }
-
-        dfs(root);
-
+        dfs(startNode);
         return res;
     }
 };
