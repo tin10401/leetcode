4bece95c7ccfeae1c76a3ee4e89987c2b481c68a
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-16 08:32:52 -0700

    Time: 112 ms (75.51%) | Memory: 9.3 MB (78.57%) - LeetSync

--- 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
+++ 1244-distinct-echo-substrings/distinct-echo-substrings.cpp
@@ -7,24 +7,24 @@ public:
         using ll = long long;
         ll mod1 = 1e9 + 7, mod2 = 1e9 + 33;
         int n = s.size();
-        vector<ll> hash1(n + 1, 0), hash2(n + 1, 0);
-        vector<ll> pow1(n + 1, 1), pow2(n + 1, 1);
-        
-        for (int i = 1; i <= n; ++i) {
-            hash1[i] = (hash1[i - 1] * 26 + (s[i - 1] - 'a' + 1)) % mod1;
-            hash2[i] = (hash2[i - 1] * 26 + (s[i - 1] - 'a' + 1)) % mod2;
-            pow1[i] = (pow1[i - 1] * 26) % mod1;
-            pow2[i] = (pow2[i - 1] * 26) % mod2;
+        vector<pair<ll, ll>> prefix(n + 1, {0, 0}), power(n + 1, {1, 1});
+
+        // Compute prefix hashes and powers
+        for(int i = 1; i <= n; i++) {
+            prefix[i].first = (prefix[i - 1].first * 26 + (s[i - 1] - 'a' + 1)) % mod1;
+            prefix[i].second = (prefix[i - 1].second * 26 + (s[i - 1] - 'a' + 1)) % mod2;
+            power[i].first = (power[i - 1].first * 26) % mod1;
+            power[i].second = (power[i - 1].second * 26) % mod2;
         }
 
         set<pair<ll, ll>> vis;
-        for (int len = 1; len * 2 <= n; ++len) {
-            for (int i = 0; i + 2 * len <= n; ++i) {
-                ll leftHash1 = (hash1[i + len] - hash1[i] * pow1[len] % mod1 + mod1) % mod1;
-                ll leftHash2 = (hash2[i + len] - hash2[i] * pow2[len] % mod2 + mod2) % mod2;
-                ll rightHash1 = (hash1[i + 2 * len] - hash1[i + len] * pow1[len] % mod1 + mod1) % mod1;
-                ll rightHash2 = (hash2[i + 2 * len] - hash2[i + len] * pow2[len] % mod2 + mod2) % mod2;
-                if (leftHash1 == rightHash1 && leftHash2 == rightHash2) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; i - j >= 0 && i + j + 1 < n; j++) {
+                ll leftHash1 = (prefix[i + 1].first - (prefix[i - j].first * power[j + 1].first % mod1) + mod1) % mod1;
+                ll leftHash2 = (prefix[i + 1].second - (prefix[i - j].second * power[j + 1].second % mod2) + mod2) % mod2;
+                ll rightHash1 = (prefix[i + j + 2].first - (prefix[i + 1].first * power[j + 1].first % mod1) + mod1) % mod1;
+                ll rightHash2 = (prefix[i + j + 2].second - (prefix[i + 1].second * power[j + 1].second % mod2) + mod2) % mod2;
+                if(leftHash1 == rightHash1 && leftHash2 == rightHash2) {
                     vis.insert({leftHash1, leftHash2});
                 }
             }
