fe238fdb76699f76d4a7001bbb726f618a0e0047
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-16 08:51:10 -0700

    Time: 19 ms (27.08%) | Memory: 14.9 MB (49.33%) - LeetSync

--- 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     using ll = long long;
-    ll dp[15][1 << 14];
-    vector<int> nums1, nums2;
-
-    ll dfs(int index, int mask) {
-        if (index == nums1.size()) return 0;
+    ll dp[15][1 << 14 + 1];
+    ll dfs(int index, int mask)
+    {
+        if(index == nums1.size()) return 0;
         ll& res = dp[index][mask];
-        if (res != -1) return res;
+        if(res != -1) return res;
         res = INT_MAX;
-        for (int i = 0; i < nums2.size(); i++) {
-            if (mask & (1 << i)) {
+        for(int i = 0; i < nums2.size(); i++)
+        {
+            if((mask >> i) & 1)
+            {
                 res = min(res, (nums1[index] ^ nums2[i]) + dfs(index + 1, mask ^ (1 << i)));
             }
-        }
+        }  
         return res;
     }
-
+    vector<int> nums1, nums2;
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
         memset(dp, -1, sizeof(dp));
-        this->nums1 = nums1;
-        this->nums2 = nums2;
         int n = nums1.size();
+        this->nums1 = nums1, this->nums2 = nums2;
         return dfs(0, (1 << n) - 1);
     }
 };
