86ce2c3ffc1a4de74fe20056298a003c31737a0a
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-17 08:24:44 -0700

    Time: 218 ms (42.20%) | Memory: 119.7 MB (21.10%) - LeetSync

--- 2018-minimum-space-wasted-from-packaging/minimum-space-wasted-from-packaging.cpp
+++ 2018-minimum-space-wasted-from-packaging/minimum-space-wasted-from-packaging.cpp
@@ -7,23 +7,23 @@ public:
         int n = packages.size();
         vector<ll> prefix(n + 1);
         for(int i = 1; i <= n; i++) prefix[i] = prefix[i - 1] + packages[i - 1];
-        auto compute = [&](vector<int>& box) {
+        auto compute = [&](vector<int>& box)
+        {
             if(packages.back() > box.back()) return LLONG_MAX;
             ll prev = 0, total = 0;
-            for(auto& b : box) {
-                auto it = upper_bound(begin(packages) + prev, end(packages), b);
-                ll next = distance(begin(packages), it);
+            for(auto& b : box)
+            {
+                ll next = upper_bound(begin(packages) + prev, end(packages), b) - begin(packages);
                 total += (ll)b * (next - prev) - (prefix[next] - prefix[prev]);
-                prev = next;
+                prev = next; 
             }
             return total;
         };
-        
-        for(auto& box : boxes) {
+        for(auto& box : boxes)
+        {
             sort(begin(box), end(box));
             res = min(res, compute(box));
         }
-        
         return res == LLONG_MAX ? -1 : res % mod;
     }
 };
