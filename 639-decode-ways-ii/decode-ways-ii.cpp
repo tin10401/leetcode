ac1ec953aa8513832e70d2088e1c3d431d56df2a
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-16 18:53:55 -0700

    Time: 25 ms (95.99%) | Memory: 13.9 MB (92.35%) - LeetSync

--- 639-decode-ways-ii/decode-ways-ii.cpp
+++ 639-decode-ways-ii/decode-ways-ii.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
     using ll = long long;
-    ll dp[100001];
     int numDecodings(string s) {
-        memset(dp, -1, sizeof(dp));
-        return dfs(0, s);
-    }
-    int mod = 1e9 + 7;
-    ll dfs(int i, const string& s)
-    {
-        if(i == s.size()) return 1;
-        if(s[i] == '0') return 0;
-        ll& res = dp[i];
-        if(res != -1) return res;
-        res = (dfs(i + 1, s) * (s[i] == '*' ? 9 : 1)) % mod;
-        if(i < s.size() - 1)
+        ll curr = 1;
+        ll next = 0;
+        int mod = 1e9 + 7;
+        int n = s.size();
+        for(int i = n - 1; i >= 0; i--)
         {
-            if(s[i] == '*' && s[i + 1] == '*')
+            if(s[i] == '0') next = curr, curr = 0;
+            else
             {
-                res += dfs(i + 2, s) * 15;
-                res %= mod;
-            }
-            else if(s[i] == '*')
-            {
-                res += dfs(i + 2, s) * (s[i + 1] <= '6' ? 2 : 1);
-                res %= mod;
-            }
-            else if(s[i + 1] == '*')
-            {
-                res += dfs(i + 2, s) * (s[i] == '1' ? 9 : (s[i] == '2' ? 6 : 0));
-                res %= mod;
-            }
-            else if(s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))
-            {
-                res += dfs(i + 2, s);
-                res %= mod;
+                ll temp = next;
+                next = curr;
+                curr = (curr * (s[i] == '*' ? 9 : 1)) % mod;
+                if(i < n - 1)
+                {
+                    if(s[i] == '*' && s[i + 1] == '*')
+                    {
+                        curr += temp * 15;
+                        curr %= mod;
+                    }
+                    else if(s[i] == '*')
+                    {
+                        curr += temp * (s[i + 1] <= '6' ? 2 : 1);
+                        curr %= mod;
+                    }
+                    else if(s[i + 1] == '*')
+                    {
+                        curr += temp * (s[i] == '1' ? 9 : (s[i] == '2' ? 6 : 0));
+                        curr %= mod;
+                    }
+                    else if(s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))
+                    {
+                        curr += temp;
+                        curr %= mod;
+                    }
+                }
             }
         }
-        return res % mod;
+        return curr;
     }
 };
