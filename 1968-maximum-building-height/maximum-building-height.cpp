abb3380cfad4d67307c3956b976c4ae451f1fd4e
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-17 12:17:59 -0700

    Time: 268 ms (84.93%) | Memory: 106.4 MB (45.21%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.cpp
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -3,8 +3,7 @@ public:
     int maxBuilding(int n, vector<vector<int>>& r) {
         r.insert(end(r), {{1, 0}, {n, n - 1}});
         sort(begin(r), end(r));
-        int res = 0;
-        int m = r.size();
+        int res = 0, m = r.size();
         for(int i = 1; i < m; i++)
         {
             r[i][1] = min(r[i][1], r[i - 1][1] + r[i][0] - r[i - 1][0]);
@@ -13,13 +12,10 @@ public:
         {
             r[i][1] = min(r[i][1], r[i + 1][1] + r[i + 1][0] - r[i][0]);
         }
-        for(int i = 0; i < m - 1; i++) 
+        for(int i = 0; i < m - 1; i++)
         {
-            int currId = r[i][0], currHeight = r[i][1];
-            int nextId = r[i + 1][0], nextHeight = r[i + 1][1];
-            int d = nextId - currId;
-            int maxHeight = (currHeight + nextHeight + d) / 2;
-            res = max(res, maxHeight);
+            int height = (r[i][1] + r[i + 1][1] + r[i + 1][0] - r[i][0]) / 2;
+            res = max(res, height);
         }
         return res;
     }
