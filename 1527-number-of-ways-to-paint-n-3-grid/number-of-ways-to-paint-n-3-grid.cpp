49d89b42665c150985e0bcc2878b04c6a4b8ddfa
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-16 17:43:11 -0700

    Time: 50 ms (25.21%) | Memory: 10.3 MB (32.60%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int dp[5001][65], m, n, mod = 1e9 + 7;
+    int setMask(int mask, int val, int index)
+    {
+        return mask | (val << (2 * index));
+    }
+    int getMask(int mask, int index)
+    {
+        return (mask >> (2 * index)) & 3;
+    }
+    int dfs(int row, int col, int prevMask, int currMask)
+    {
+        if(row == n) return 1;
+        if(col == m) return dfs(row + 1, 0, currMask, 0);
+        if(col == 0 && dp[row][prevMask] != -1) return dp[row][prevMask];
+        int res = 0;
+        for(int i = 1; i <= 3; i++)
+        {
+            if(i != getMask(prevMask, col) && (col == 0 || i != getMask(currMask, col - 1)))
+            {
+                res = (res + dfs(row, col + 1, prevMask, setMask(currMask, i, col))) % mod;
+            }
+        }
+        dp[row][prevMask] = res;
+        return res;
+    }
+
+
+    int numOfWays(int n, int m = 3) {
+        memset(dp, -1, sizeof(dp));
+        this->m = m, this->n = n;
+        return dfs(0, 0, 0, 0);
+    }
+};
