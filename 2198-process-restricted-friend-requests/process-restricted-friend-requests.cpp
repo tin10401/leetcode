a92915451f4f2b2efd1024fac1a277f0481352aa
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-18 11:25:55 -0700

    Time: 26 ms (99.61%) | Memory: 31 MB (53.70%) - LeetSync

--- 2198-process-restricted-friend-requests/process-restricted-friend-requests.cpp
+++ 2198-process-restricted-friend-requests/process-restricted-friend-requests.cpp
@@ -1,10 +1,9 @@
 class UnionFind
 {
     public:
-    vector<unordered_set<int>> ban;
-    unordered_set<int> rootSet;
+    vector<bitset<1000>> ban;
     int n;
-    vector<unordered_set<int>> children;
+    vector<bitset<1000>> children;
     vector<int> root, rank;
     UnionFind(int n, vector<vector<int>>& restrictions)
     {
@@ -13,12 +12,12 @@ class UnionFind
         for(auto& it : restrictions)
         {
             int u = it[0], v = it[1];
-            ban[u].insert(v);
-            ban[v].insert(u);
+            ban[u].set(v);
+            ban[v].set(u);
         }
         for(int i = 0; i < n; i++)
         {
-            children[i].insert(i);
+            children[i].set(i);
         }
     }
 
@@ -29,26 +28,15 @@ class UnionFind
         return root[x] = find(root[x]);
     }
 
-    bool search(unordered_set<int>& a, unordered_set<int>& b)
-    {
-        for(auto& n : a)
-        {
-            for(auto& all : ban[n])
-            {
-                if(b.count(all)) return false;
-            }
-        }
-        return true;
-    }
-
     bool merge(int x, int y)
     {
         int u = find(x), v = find(y);
         if(u == v) return true;
-        if(!search(children[u], children[v])) return false;
+        if((children[u] & ban[v]).any() || (children[v] & ban[u]).any()) return false;
         if(rank[v] > rank[u]) swap(u, v);
         rank[u] += rank[v];
-        children[u].insert(children[v].begin(), children[v].end());
+        children[u] |= children[v];
+        ban[u] |= ban[v];
         root[v] = u;
         return true;
     }
