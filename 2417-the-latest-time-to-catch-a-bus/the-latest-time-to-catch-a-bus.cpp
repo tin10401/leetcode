c98566b6563fef6e6f0f89be35baf3806e377924
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-17 19:05:51 -0700

    Time: 249 ms (5.12%) | Memory: 88.5 MB (14.93%) - LeetSync

---
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int latestTimeCatchTheBus(vector<int>& buses, vector<int>& passengers, int capacity) {
+        sort(begin(buses), end(buses));
+        sort(begin(passengers), end(passengers));
+        auto check = [&](int n)
+        {
+            int i = 0;
+            for(auto& b : buses)
+            {
+                int current = 0;
+                while(i < passengers.size() && passengers[i] <= min(n, b) && current < capacity) i++, current++;
+                if(current == capacity) continue;
+                if(n <= b) return true;
+            }
+            return false;
+        };
+        int left = 0, right = buses.back(), res = -1;
+        while(left <= right)
+        {
+            int middle = left + (right - left) / 2;
+            if(check(middle)) res = middle, left = middle + 1;
+            else right = middle - 1;
+        }
+        unordered_set<int> s(begin(passengers), end(passengers));
+        while(s.count(res)) res--;
+        return res;
+    }
+};
