8c5485d7f32b9ac2feb45743b95559d1b3f30669
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-18 19:57:32 -0700

    Time: 2116 ms (6.40%) | Memory: 43.7 MB (88.35%) - LeetSync

---
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minTrioDegree(int n, vector<vector<int>>& edges) {
+        vector<vector<bool>> vis(n, vector<bool>(n));
+        int count[n];
+        memset(count, 0, sizeof(count));
+        for(auto& edge : edges)
+        {
+            int u = edge[0] - 1, v = edge[1] - 1;
+            count[u]++;
+            count[v]++;
+            vis[u][v] = vis[v][u] = true;
+        }
+        int res = INT_MAX;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                for(int k = 0; k < n; k++)
+                {
+                    if(vis[i][j] && vis[i][k] && vis[j][k])
+                    {
+                        res = min(res, count[i] + count[j] + count[k] - 6);
+                    }
+                }
+            }
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+};
