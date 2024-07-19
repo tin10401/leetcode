d59d25a76f96234bd1dcb3bfae8091c97f5568fb
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-19 11:09:19 -0700

    Time: 677 ms (30.74%) | Memory: 297.3 MB (10.95%) - LeetSync

--- 2717-collect-coins-in-a-tree/collect-coins-in-a-tree.cpp
+++ 2717-collect-coins-in-a-tree/collect-coins-in-a-tree.cpp
@@ -2,54 +2,42 @@ class Solution {
 public:
     int collectTheCoins(vector<int>& coins, vector<vector<int>>& edges) {
         int n = coins.size();
-        vector<unordered_set<int>> tree(n);
-        for(int i = 0; i < n; i++)
+        vector<unordered_set<int>> graph(n);
+        for(auto& edge : edges)
         {
-            tree[i] = unordered_set<int>();
-        }
-
-        for(auto& e : edges)
-        {
-            tree[e[0]].insert(e[1]);
-            tree[e[1]].insert(e[0]);
+            graph[edge[0]].insert(edge[1]);
+            graph[edge[1]].insert(edge[0]);
         }
         vector<int> leaf;
         for(int i = 0; i < n; i++)
         {
             int u = i;
-            while(tree[u].size() == 1 && coins[u] == 0)
+            while(graph[u].size() == 1 && coins[u] == 0)
             {
-                int v = *tree[u].begin();
-                tree[u].erase(v);
-                tree[v].erase(u);
+                int v = *graph[u].begin();
+                graph[u].erase(v);
+                graph[v].erase(u);
                 u = v;
             }
-            if(tree[u].size() == 1)
-            {
-                leaf.push_back(u);
-            }
+            if(graph[u].size() == 1) leaf.push_back(u);
         }
-
-        for(int sz = 2; sz; sz--)
+        for(int i = 0; i < 2; i++)
         {
-            vector<int> temp;
+            vector<int> next;
             for(auto& u : leaf)
             {
-                if(tree[u].size() == 1)
+                if(graph[u].size() == 1)
                 {
-                    int v = *tree[u].begin();
-                    tree[u].erase(v);
-                    tree[v].erase(u);
-                    if(tree[v].size() == 1) temp.push_back(v);
+                    int v = *graph[u].begin();
+                    graph[v].erase(u);
+                    graph[u].erase(v);
+                    if(graph[v].size() == 1) next.push_back(v);
                 }
             }
-            leaf = temp;
+            leaf = next;
         }
         int res = 0;
-        for(int i = 0; i < n; i++)
-        {
-            res += tree[i].size();
-        }
+        for(int i = 0; i < n; i++) res += graph[i].size();
         return res;
     }
 };
