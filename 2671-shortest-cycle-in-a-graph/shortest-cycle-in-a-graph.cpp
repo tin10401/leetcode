ec86990d9e3882b413d1b9d13d362e3ebdb7803a
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-19 13:36:17 -0700

    Time: 232 ms (77.99%) | Memory: 157.2 MB (47.66%) - LeetSync

---
+++ 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findShortestCycle(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        for(auto& edge : edges)
+        {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        int res = INT_MAX;
+        for(int i = 0; i < n; i++)
+        {
+            vector<int> dis(n, INT_MAX);
+            dis[i] = 0;
+            queue<int> q;
+            q.push(i);
+            vector<int> parent(n, -1);
+            while(!q.empty())
+            {
+                int node = q.front();
+                q.pop();
+                for(auto& nei : graph[node])
+                {
+                    if(dis[node] + 1 < dis[nei]) 
+                    {
+                        dis[nei] = dis[node] + 1;
+                        parent[nei] = node;
+                        q.push(nei);
+                    }
+                    else 
+                    {
+                        if(parent[nei] != node && parent[node] != nei)
+                        {
+                            res = min(res, dis[nei] + dis[node] + 1);
+                        }
+                    }
+                }
+            }
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+};
