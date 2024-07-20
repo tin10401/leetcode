0d671b89d484c110ecf627d5506f1f95a6305eb0
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-19 17:18:59 -0700

    Time: 70 ms (100.00%) | Memory: 153.4 MB (68.00%) - LeetSync

--- 3521-count-triplets-with-even-xor-set-bits-ii/count-triplets-with-even-xor-set-bits-ii.cpp
+++ 3521-count-triplets-with-even-xor-set-bits-ii/count-triplets-with-even-xor-set-bits-ii.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     long long tripletCount(vector<int>& a, vector<int>& b, vector<int>& c) {
+        // SOLUTION OF TIN LE
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         using ll = long long;
         ll bits[3][2] = {};
         for(auto& num : a) bits[0][__builtin_popcount(num) % 2]++;
