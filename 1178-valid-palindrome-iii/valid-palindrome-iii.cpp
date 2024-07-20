ef949e9c075398306749261c4dd42b7439a22ff7
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-19 18:48:32 -0700

    Time: 11 ms (72.08%) | Memory: 9 MB (91.72%) - LeetSync

---
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidPalindrome(string s, int k) {
+        int n = s.size();
+        string ss = string(rbegin(s), rend(s));
+        int dp[n + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(s[i - 1] == ss[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return (s.size() - dp[n][n]) <= k;
+    }
+};
