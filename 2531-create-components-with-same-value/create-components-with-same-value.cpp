7057449410831b35738e88eb8f028fb2742df48d
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-23 10:00:53 -0700

    Time: 278 ms (81.14%) | Memory: 127.7 MB (89.14%) - LeetSync

--- 2531-create-components-with-same-value/create-components-with-same-value.cpp
+++ 2531-create-components-with-same-value/create-components-with-same-value.cpp
@@ -1,52 +1,33 @@
 class Solution {
 public:
-    bool dfs2(int node, int par, int per, int left)
-    {
-        if(left == 0) left = per;
-        if(nums[node] > left) return false;
-        int total = 0;
-        for(auto& nei : graph[node])
-        {
-            if(nei != par)
-            {
-                int nLeft = sum[nei] % per;
-                total += nLeft;
-                if(!dfs2(nei, node, per, nLeft)) return false;
-            }
-        }
-        return (total + nums[node]) == left;
-    }
-    int dfs(int node, int par)
-    {
-        int total = nums[node];
-        for(auto& nei : graph[node])
-        {
-            if(nei != par) total += dfs(nei, node);
-        }
-        sum[node] = total;
-        return total;
-    }
-    vector<int> nums, sum;
-    vector<vector<int>> graph;
     int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
-        this->nums = nums;
         int n = nums.size();
-        graph.resize(n), sum.resize(n);
-        for(auto& edge : edges)
-        {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+        vector<vector<int>> tree(n);
+        for(auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            tree[u].push_back(v);
+            tree[v].push_back(u);
         }
-        dfs(0, -1);
-        int total = accumulate(begin(nums), end(nums), 0);
-        vector<int> list;
-        for(int i = 1; i <= total; i++)
-        {
-            if(total % i == 0)
-            {
-                if(dfs2(0, -1, i, 0)) return (total / i) - 1; 
+
+        auto fn = [&](int u, int p, int cand, auto& fn) -> int {
+            int ans = nums[u];
+            for(int v : tree[u]) {
+                if (v != p) {
+                    ans += fn(v, u, cand, fn);
+                }
             }
-        }     
+            return (ans == cand) ? 0 : ans;
+        };
+
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        for(int cand = 1; cand <= total / 2; ++cand) {
+            if (total % cand == 0) {
+                int result = fn(0, -1, cand, fn);
+                if (result == 0) {
+                    return total / cand - 1;
+                }
+            }
+        }
         return 0;
     }
 };
