ceea5215e70f41a6762210edddcf2788497a6edf
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-07-23 10:06:38 -0700

    Time: 291 ms (78.86%) | Memory: 127.8 MB (85.71%) - LeetSync

--- 2531-create-components-with-same-value/create-components-with-same-value.cpp
+++ 2531-create-components-with-same-value/create-components-with-same-value.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
         int n = nums.size();
-        vector<vector<int>> tree(n);
-        for(auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-            tree[u].push_back(v);
-            tree[v].push_back(u);
+        vector<vector<int>> graph(n);
+        for(auto& edge : edges)
+        {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
-
-        auto fn = [&](int u, int p, int cand, auto& fn) -> int {
-            int ans = nums[u];
-            for(int v : tree[u]) {
-                if (v != p) {
-                    ans += fn(v, u, cand, fn);
+        auto dfs = [&](int node, int par, int cand, auto& dfs) -> int
+        {
+            int total = nums[node];
+            for(auto& nei : graph[node])
+            {
+                if(nei != par)
+                {
+                    total += dfs(nei, node, cand, dfs);
                 }
-            }
-            return (ans == cand) ? 0 : ans;
+            } 
+            return total == cand ? 0 : total;
         };
-
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        for(int cand = 1; cand <= total / 2; ++cand) {
-            if (total % cand == 0) {
-                int result = fn(0, -1, cand, fn);
-                if (result == 0) {
-                    return total / cand - 1;
-                }
+        int sum = accumulate(begin(nums), end(nums), 0);
+        for(int i = 1; i < sum; i++)
+        {
+            if(sum % i == 0) 
+            {
+                int ans = dfs(0, -1, i, dfs);
+                if(ans == 0) return sum / i - 1;
             }
         }
         return 0;
