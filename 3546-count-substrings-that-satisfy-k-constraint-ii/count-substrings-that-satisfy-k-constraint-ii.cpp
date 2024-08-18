6fe9823fb8dc3fe37273f29fd11722205b1eda38
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-18 08:10:16 -0700

    Time: 597 ms (35.71%) | Memory: 186.7 MB (28.57%) - LeetSync

---
+++ 3546-count-substrings-that-satisfy-k-constraint-ii/count-substrings-that-satisfy-k-constraint-ii.cpp
@@ -0,0 +1,193 @@
+// Author : Tin Le
+//   __________________
+//  | ________________ |
+//  ||          ____  ||
+//  ||   /\    |      ||
+//  ||  /__\   |      ||
+//  || /    \  |____  ||
+//  ||________________||
+//  |__________________|
+//  \###################\
+//   \###################\
+//    \        ____       \
+//     \_______\___\_______\
+// An AC a day keeps the doctor away.
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define all(x) begin(x), end(x)
+#define allr(x) rbegin(x), rend(x)
+#define ub upper_bound
+#define lb lower_bound
+#define db double
+#define ll long long
+#define vll vector<ll>
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvpii vector<vpii>
+#define vvi vector<vi>
+#define vd vector<db>
+#define ar(x) array<int, x>
+#define var(x) vector<ar(x)>
+#define pq priority_queue
+#define mset(m, v) memset(m, v, sizeof(m))
+#define pb push_back
+#define ff first
+#define ss second
+#define MP make_pair
+#define rsz resize
+#define sum(x) accumulate(all(x), 0LL)
+#define srt(x) sort(all(x))
+#define srtR(x) sort(allr(x))
+#define srtU(x) sort(all(x)), (x).erase(unique(all(x)), (x).end())
+#define rev(x) reverse(all(x))
+
+//SGT DEFINE
+#define lc i * 2 + 1
+#define rc i * 2 + 2
+#define lp lc, left, middle
+#define rp rc, middle + 1, right
+#define entireTree 0, 0, n - 1
+#define midPoint left + (right - left) / 2
+#define pushDown push(i, left, right)
+#define iterator int i, int left, int right
+    
+//FW TREE   
+#define goUp id += (id & -id)   
+#define goDown id -= (id & -id)
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#ifdef LOCAL
+#define startClock clock_t tStart = clock();
+#define endClock cout << fixed << setprecision(10) << "\nTime Taken: " << (double)(clock() - tStart) / CLOCKS_PER_SEC << endl;
+#else
+#define startClock
+#define endClock
+#endif
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const static ll INF = 1LL << 61;
+const static int MX = 2e6 + 5;
+const static int MOD = 1e9 + 7;
+const static string no = "NO\n";
+const static string yes = "YES\n";
+constexpr int pct(int x) { return __builtin_popcount(x); }
+const vvi dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+constexpr int modExpo(int base, int exp, int mod) { int res = 1; while(exp) { if(exp & 1) res = (res * base) % mod; base = (base * base) % mod; exp >>= 1; } return res; }
+void multiply(int f[2][2], int m[2][2]) {   
+    int res[2][2] = {}; 
+    for(int i = 0; i < 2; i++)  {   for(int j = 0; j < 2; j++)  {   for(int k = 0; k < 2; k++)  {   res[i][j] = (res[i][j] + f[i][k] * m[k][j]) % MOD; }   }   }   
+    for(int i = 0; i < 2; i++)  {   for(int j = 0; j < 2; j++) f[i][j] = res[i][j]; }   }
+int fib(int n)  {       if(n == 0) return 0;        if(n == 1) return 1;    
+    int f[2][2] = {{1, 1}, {1, 0}}; int res[2][2] = {{1, 0}, {0, 1}};       
+    while(n)    {   if(n & 1) multiply(res, f); multiply(f, f); n >>= 1;    }   return res[0][1] % MOD; }   
+int GCD[MX], TOTI[MX];  
+void gcdSum()  {   for(int i = 0; i < MX; i++) TOTI[i] = i;   
+    for(int i = 2; i < MX; i++) {   if(TOTI[i] == i)   {   TOTI[i] = i - 1; for(int j = 2 * i; j < MX; j += i)  {   TOTI[j] -= (TOTI[j] / i); }   }   }   
+    for(int i = 1; i < MX; i++) {   for(int j = i, k = 1; j < MX; j += i, k++)  {   GCD[j] += i * TOTI[k];   }   }
+}
+struct custom {
+    static const uint64_t C = 0x9e3779b97f4a7c15; const uint32_t RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+    size_t operator()(uint64_t x) const { return __builtin_bswap64((x ^ RANDOM) * C); }
+    size_t operator()(const std::string& s) const { size_t hash = std::hash<std::string>{}(s); return hash ^ RANDOM; } };
+template <class K, class V> using umap = std::unordered_map<K, V, custom>; template <class K> using uset = std::unordered_set<K, custom>;
+class SGT
+{
+    public:
+    int n;
+    vll root, lazy;
+    SGT(int n)
+    {
+        this->n = n;
+        root.rsz(n * 4), lazy.rsz(n * 4);
+    }
+
+    void update(int start, int end, ll val)
+    {
+        update(entireTree, start, end, val);
+    }
+
+    void update(iterator, int start, int end, ll val)
+    {
+        pushDown;
+        if(left > end || start > right) return;
+        if(left >= start && right <= end)
+        {
+            lazy[i] += val;
+            pushDown;
+            return;
+        }
+        int middle = midPoint;
+        update(lp, start, end, val);
+        update(rp, start, end, val);
+        root[i] = root[lc] + root[rc];
+    }
+
+    ll queries(int start, int end)
+    {
+        return queries(entireTree, start, end);
+    }
+
+    ll queries(iterator, int start, int end)
+    {
+        pushDown;
+        if(left >= start && right <= end) return root[i];
+        if(left > end || start > right) return 0;
+        int middle = midPoint;
+        return queries(lp, start, end) + queries(rp, start, end);
+    }
+
+    void push(int i, int left, int right)
+    {
+        if(lazy[i] == 0) return;
+        root[i] += (right - left + 1) * lazy[i];
+        
+        if(left != right)
+        {
+            lazy[lc] += lazy[i];
+            lazy[rc] += lazy[i];
+        }
+        lazy[i] = 0;
+    }
+};
+class Solution {
+public:
+    vector<long long> countKConstraintSubstrings(string s, int k, vector<vector<int>>& queries) {
+        int n = s.size();
+        SGT root(n);
+        int m = queries.size();
+        vll res(m);
+        vpii Q[n];
+        for(int i = 0; i < m; i++)
+        {
+            Q[queries[i][1]].pb({queries[i][0], i});
+        }
+        ll ans = 0;
+        for(int left = 0, right = 0, one = 0, zero = 0; right < n; right++)
+        {
+            if(s[right] == '0') zero++;
+            else one++;
+            while(one > k && zero > k)
+            {
+                if(s[left++] == '0') zero--;
+                else one--;
+            }
+            root.update(left, right, 1);
+            ans += right - left + 1;
+            for(auto& [l, index] : Q[right])
+            {
+                res[index] += ans - (l == 0 ? 0 : root.queries(0, l - 1));
+            }
+        }
+        return res;
+    }
+};
