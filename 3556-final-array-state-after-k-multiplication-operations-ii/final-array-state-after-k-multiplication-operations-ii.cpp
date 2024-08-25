5c41120db596b6a29cb56b604f6ff8e7450a0b59
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-24 22:52:33 -0700

    Time: 189 ms (50.00%) | Memory: 82.7 MB (57.14%) - LeetSync

--- 3556-final-array-state-after-k-multiplication-operations-ii/final-array-state-after-k-multiplication-operations-ii.cpp
+++ 3556-final-array-state-after-k-multiplication-operations-ii/final-array-state-after-k-multiplication-operations-ii.cpp
@@ -1,92 +1,40 @@
-int mod=1e9+7;
-
 class Solution {
 public:
-    long long modexp(long long x, long long y) 
-    { 
-        int res = 1;    
-        x = x % mod;   
-        if (x == 0) return 0;
-        while (y > 0) 
-        { 
-            if (y & 1) 
-                res = (res*x) % mod; 
-            y = y>>1;
-            x = (x*x) % mod; 
-        } 
-        return res; 
-    } 
-
-    vector<int> getFinalState(vector<int>& nums, int k, int m) {
-        if(m==1) return nums;
-
-        int n=nums.size();
-        vector<pair<int,int>> list(n);
-        for(int i=0; i<n; i++) list[i].first=nums[i], list[i].second=i;
-        vector<int> raise(n, 0);
-
-        sort(list.begin(), list.end());
-        if(n>1)
+#define ll long long
+#define ff first
+#define ss second
+#define pll pair<ll, ll>
+    ll modExpo(ll base, ll exp, ll mod)
+    {
+        ll res = 1;
+        while(exp)
         {
-            int l=1, r=list[n-1].first;
-            int req=0;
-            while(l<=r)
-            {
-                int mid=l+(r-l)/2;
-                int count=0;
-                for(int i=0; i<n; i++)
-                {
-                    int x=list[i].first;
-                    int z=floor((log2(mid)-log2(x))/log2(m)+1e-6);
-                    if(z<=0) break;
-                    count+=z;
-                }
-                if(count>k) r=mid-1;
-                else if(count<k)
-                {
-                    req=max(req, mid);
-                    l=mid+1;
-                }
-                else
-                {
-                    req=max(req, mid);
-                    break;
-                }
-            }
-
-            for(int i=0; i<n && k>0; i++)
-            {
-                int x=list[i].first;
-                int z=floor((log2(req)-log2(x))/log2(m)+1e-6);
-                if(z<=0) break;
-                raise[i]+=z;
-                k-=z;
-            }
+            if(exp & 1) res = (res * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
         }
-
-
-
-        for(int i=0; i<n; i++)
+        return res;
+    }
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        if(multiplier == 1) return nums;
+        set<pll> s;
+        int MOD = 1e9 + 7, n = nums.size();
+        for(int i = 0; i < n; i++) s.insert({(ll)nums[i], (ll)i});
+        while(s.begin()->ff * multiplier <= s.rbegin()->ff && k)
         {
-            list[i].first*=modexp(m, raise[i]);
+            k--;
+            pll tmp = *s.begin();
+            s.erase(tmp);
+            s.insert({(tmp.ff * multiplier), tmp.ss});
         }
-
-        sort(list.begin(), list.end());
-        
-        int a=k/n, b=k%n;
-        // vector<pair<int,int>> list(ord.begin(), ord.end());
-
-        for(int i=0; i<b; i++)
+        int take = k / n;
+        k %= n;
+        for(auto& [a, f] : s)
         {
-            list[i].first=(list[i].first*modexp(m, a+1))%mod;
+            ll b = (a * modExpo(multiplier, take, MOD) % MOD * (k-- > 0 ? multiplier : 1)) % MOD;
+            nums[f] = b;
         }
-        for(int i=b; i<n; i++)
-        {
-            list[i].first=(list[i].first*modexp(m, a))%mod;
-        }
-
-        for(auto &el:list) nums[el.second]=el.first;
-
         return nums;
+
     }
 };
