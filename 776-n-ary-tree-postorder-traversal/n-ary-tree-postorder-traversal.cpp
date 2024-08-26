8612db4a7180f2dead66aa72437e49979f78cc6f
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-25 17:33:28 -0700

    Time: 11 ms (78.54%) | Memory: 15.2 MB (70.21%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -20,22 +20,23 @@ public:
 
 class Solution {
 public:
-    vector<int> list;
     vector<int> postorder(Node* root) {
-        if(root == nullptr)
+        if(!root) return {};
+        vector<int> res;
+        stack<Node*> s;
+        s.push(root);
+        while(!s.empty())
         {
-            return list;
-        }
-        if(!root->children.empty())
-        {
-            int size = root->children.size();
-            for(int i = 0; i < size; i++)
+            auto node = s.top();
+            bool ok = false;
+            int n = node->children.size();
+            for(int i = n - 1; i >= 0; i--)
             {
-                postorder(root->children[i]);
+                auto nei = node->children[i];
+                if(nei->val != INT_MIN) {ok = true, s.push(nei);}
             }
-            
+            if(!ok) {res.push_back(node->val); node->val = INT_MIN; s.pop();}
         }
-        list.push_back(root->val);
-        return list;
+        return res;
     }
 };
