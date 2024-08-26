40be5074dad77d2881551764300f18c59e7eff7d
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-26 12:22:03 -0700

    Time: 1202 ms (11.11%) | Memory: 149.7 MB (44.44%) - LeetSync

--- 3544-count-almost-equal-pairs-ii/count-almost-equal-pairs-ii.cpp
+++ 3544-count-almost-equal-pairs-ii/count-almost-equal-pairs-ii.cpp
@@ -1,52 +1,36 @@
 class Solution {
-    string makeDigit(int num, int digits) {
-        string s = to_string(num);
-        int n = s.size();
-        for(int i=0;i<digits-n;i++)
-            s = "0" + s;
-        return s;
-    }
-
-    unordered_set<string> makeSwapChanges(int num, int digits) {
-        string s = makeDigit(num, digits);
-        unordered_set<string> poss;
-        poss.insert(s);
-        
-        for(int i = 0; i < digits; ++i) {
-            for(int j = i + 1; j < digits; ++j) {
-                swap(s[i], s[j]);
-                poss.insert(s);
-                for(int i1 = 0; i1 < digits; ++i1) {
-                    for(int j1 = i1+1; j1 < digits; ++j1) {
-                        if(s[i1] != s[j1]) {
-                            swap(s[i1], s[j1]);
-                            poss.insert(s);
-                            swap(s[j1], s[i1]);
-                        }
-                    }
-                }
-                swap(s[i], s[j]);
-            }
-        }
-        return poss;
-    }
-
 public:
     int countPairs(vector<int>& nums) {
         int n = nums.size();
-        int digits = to_string(*max_element(nums.begin(),nums.end())).size();
-
-        unordered_map<string, int> mp;
+        sort(nums.begin(), nums.end(), greater());
         int ans = 0;
-        for(int i = 0; i < n; ++i) {
-            for(const auto& s : makeSwapChanges(nums[i], digits)) {
-                if(mp.count(s)) {
-                    ans += mp[s];
+        // for (auto i : nums) cout << i << " ";
+        // cout << endl;
+        unordered_map<int, int> count;
+        unordered_set<int> s;
+        for (int index = 0; index < n; index++){
+            int c = 1;
+            while (index + 1 < n && nums[index + 1] == nums[index]) c++, index++;
+            ans += c * (c - 1) / 2;
+            ans += count[nums[index]] * c;
+            string curr = to_string(nums[index]);
+            int k = curr.length();
+            for (int i = 0; i < k; i++){
+                for (int j = i; j < k; j++){
+                    swap(curr[i], curr[j]);
+                    for (int a = 0; a < k; a++){
+                        for (int b = a; b < k; b++){
+                            swap(curr[a], curr[b]);
+                            s.insert(stoi(curr));
+                            swap(curr[a], curr[b]);
+                        }
+                    }
+                    swap(curr[i], curr[j]);
                 }
             }
-            mp[makeDigit(nums[i], digits)]++;
+            for (int temp : s) count[temp] += c;
+            s.clear();
         }
-
         return ans;
     }
 };
