3a46b034fb17254c49213de9ec6a1a0d1310e031
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-26 12:22:56 -0700

    Time: 2255 ms (11.11%) | Memory: 500.3 MB (11.11%) - LeetSync

--- 3544-count-almost-equal-pairs-ii/count-almost-equal-pairs-ii.cpp
+++ 3544-count-almost-equal-pairs-ii/count-almost-equal-pairs-ii.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
-    int countPairs(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end(), greater());
-        int ans = 0;
-        // for (auto i : nums) cout << i << " ";
-        // cout << endl;
-        unordered_map<int, int> count;
-        unordered_set<int> s;
-        for (int index = 0; index < n; index++){
-            int c = 1;
-            while (index + 1 < n && nums[index + 1] == nums[index]) c++, index++;
-            ans += c * (c - 1) / 2;
-            ans += count[nums[index]] * c;
-            string curr = to_string(nums[index]);
-            int k = curr.length();
-            for (int i = 0; i < k; i++){
-                for (int j = i; j < k; j++){
-                    swap(curr[i], curr[j]);
-                    for (int a = 0; a < k; a++){
-                        for (int b = a; b < k; b++){
-                            swap(curr[a], curr[b]);
-                            s.insert(stoi(curr));
-                            swap(curr[a], curr[b]);
+    unordered_map<string,int>dp;
+    string makeDigit(int num, int digits) {//func for digitmakeup
+        string s = to_string(num);
+        int n = s.size();
+        for(int i=0;i<digits-n;i++)
+            s = "0" + s;
+        return s;
+    }
+    unordered_map<string,int>getnums(int num,int digits_makeup){
+        string s = makeDigit(num,digits_makeup);
+        unordered_map<string,int>mp;
+        mp[s]++;
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i + 1; j < s.length(); j++){
+                swap(s[i],s[j]);
+                mp[s]++;
+                for(int k = 0; k < s.length(); k++){
+                    for(int l = k + 1; l < s.length(); l++){
+                        if(s[k]!=s[l]){
+                            swap(s[k],s[l]);
+                            mp[s]++;
+                            swap(s[k],s[l]);
                         }
                     }
-                    swap(curr[i], curr[j]);
+                }
+                swap(s[i],s[j]);
+            }
+        }
+        return mp;
+    }
+    int countPairs(vector<int>& nums) {
+        // O(N*49*49) here
+        int n=nums.size();
+        int digits_makeup = to_string(*max_element(nums.begin(),nums.end())).size();
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            for(auto s:getnums(nums[i],digits_makeup)){
+                if(dp.count(s.first)){
+                    ans+=dp[s.first];
                 }
             }
-            for (int temp : s) count[temp] += c;
-            s.clear();
+            dp[makeDigit(nums[i],digits_makeup)]++;
         }
         return ans;
     }
