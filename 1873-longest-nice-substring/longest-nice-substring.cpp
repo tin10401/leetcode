bd6c8e89614a3d3b002342d7140f06032f08434c
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-27 13:56:56 -0700

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.57%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.cpp
+++ 1873-longest-nice-substring/longest-nice-substring.cpp
@@ -4,34 +4,34 @@ public:
         int n = s.size(), len = 0, start = -1;
         for(int unique = 1; unique < 26; unique++)
         {
-            int total[26] = {}, upper[26] = {}, lower[26] = {}, countLower = 0, countUpper = 0, totalCount = 0;
+            int total[26] = {}, upper[26] = {}, lower[26] = {}, countLower = 0, countUpper = 0, uniqueCount = 0;
             for(int right = 0, left = 0; right < n; right++)
             {
                 if(isupper(s[right]))
                 {
-                    totalCount += ++total[s[right] - 'A'] == 1;
+                    uniqueCount += ++total[s[right] - 'A'] == 1;
                     countUpper += ++upper[s[right] - 'A'] == 1;
                 }
                 else
                 {
-                    totalCount += ++total[s[right] - 'a'] == 1;
+                    uniqueCount += ++total[s[right] - 'a'] == 1;
                     countLower += ++lower[s[right] - 'a'] == 1;
                 }
-                while(totalCount > unique)
+                while(uniqueCount > unique)
                 {
                     if(isupper(s[left]))
                     {
-                        totalCount -= --total[s[left] - 'A'] == 0;
+                        uniqueCount -= --total[s[left] - 'A'] == 0;
                         countUpper -= --upper[s[left] - 'A'] == 0;
                     }
                     else
                     {
-                        totalCount -= --total[s[left] - 'a'] == 0;
+                        uniqueCount -= --total[s[left] - 'a'] == 0;
                         countLower -= --lower[s[left] - 'a'] == 0;
                     }
                     left++;
                 }
-                if(totalCount == countLower && countLower == countUpper && right - left + 1 > len)
+                if(uniqueCount == countLower && countLower == countUpper && right - left + 1 > len)
                 {
                     len = right - left + 1;
                     start = left;
