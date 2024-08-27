abcde50daca9b8876ee44183bbd4029396607ebd
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-27 14:32:52 -0700

    Time: 30 ms (70.48%) | Memory: 23.3 MB (30.93%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,167 @@
+//████████╗██╗███╗░░██╗  ██╗░░░░░███████╗
+//╚══██╔══╝██║████╗░██║  ██║░░░░░██╔════╝
+//░░░██║░░░██║██╔██╗██║  ██║░░░░░█████╗░░
+//░░░██║░░░██║██║╚████║  ██║░░░░░██╔══╝░░
+//░░░██║░░░██║██║░╚███║  ███████╗███████╗
+//░░░╚═╝░░░╚═╝╚═╝░░╚══╝  ╚══════╝╚══════╝
+//   __________________
+//  | ________________ |
+//  ||          ____  ||
+//  ||   /\    |      ||
+//  ||  /__\   |      ||
+//  || /    \  |____  ||
+//  ||________________||
+//  |__________________|
+//  \###################\
+//   \###################\
+//    \        ____       \
+//     \_______\___\_______\
+// An AC a day keeps the doctor away.
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define vt vector
+#define all(x) begin(x), end(x)
+#define allr(x) rbegin(x), rend(x)
+#define ub upper_bound
+#define lb lower_bound
+#define db double
+#define ll long long
+#define pll pair<ll, ll>    
+#define vll vt<ll>  
+#define vpll vt<pll>
+//#define int long long
+#define vi vector<int>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvpii vector<vpii>
+#define vvi vector<vi>
+#define vd vector<db>
+#define ar(x) array<int, x>
+#define var(x) vector<ar(x)>
+#define pq priority_queue
+#define mset(m, v) memset(m, v, sizeof(m))
+#define pb push_back
+#define ff first
+#define ss second
+#define sv string_view
+#define MP make_pair
+#define rsz resize
+#define sum(x) accumulate(all(x), 0LL)
+#define srt(x) sort(all(x))
+#define srtR(x) sort(allr(x))
+#define srtU(x) sort(all(x)), (x).erase(unique(all(x)), (x).end())
+#define rev(x) reverse(all(x))
+#define gcd(a, b) __gcd(a, b)
+
+//SGT DEFINE
+#define lc i * 2 + 1
+#define rc i * 2 + 2
+#define lp lc, left, middle
+#define rp rc, middle + 1, right
+#define entireTree 0, 0, n - 1
+#define midPoint left + (right - left) / 2
+#define pushDown push(i, left, right)
+#define iterator int i, int left, int right
+
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+    
+//FW TREE   
+#define goUp id += (id & -id)   
+#define goDown id -= (id & -id)
+    
+template<typename T1, typename T2>
+std::ostream& operator<<(std::ostream& o, const std::pair<T1, T2>& p) { return o << p.ff << " " << p.ss; }
+
+#ifdef LOCAL
+#define debug(x...) debug_out(#x, x)
+void debug_out(const char* names) { std::cerr << std::endl; }
+template <typename T, typename... Args>
+void debug_out(const char* names, T value, Args... args) {
+    const char* comma = strchr(names, ',');
+    std::cerr << "[" << (comma ? std::string(names, comma) : names) << " = " << value << "]";
+    if (sizeof...(args)) { std::cerr << ", "; debug_out(comma + 1, args...); }   
+    else { std::cerr << std::endl; }
+}
+#define startClock clock_t tStart = clock();
+#define endClock std::cout << std::fixed << std::setprecision(10) << "\nTime Taken: " << (double)(clock() - tStart) / CLOCKS_PER_SEC << " seconds" << std::endl;
+#else
+#define debug(...)
+#define startClock
+#define endClock
+
+#endif
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const static ll INF = 1LL << 61;
+const static int MX = 2e6 + 5;
+const static int MOD = 1e9 + 7;
+const static string no = "NO\n";
+const static string yes = "YES\n";
+constexpr int pct(int x) { return __builtin_popcount(x); }
+const vvi dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+constexpr int modExpo(int base, int exp, int mod) { int res = 1; while(exp) { if(exp & 1) res = (res * base) % mod; base = (base * base) % mod; exp >>= 1; } return res; }
+void multiply(int f[2][2], int m[2][2]) {   
+    int res[2][2] = {}; 
+    for(int i = 0; i < 2; i++)  {   for(int j = 0; j < 2; j++)  {   for(int k = 0; k < 2; k++)  {   res[i][j] = (res[i][j] + f[i][k] * m[k][j]) % MOD; }   }   }   
+    for(int i = 0; i < 2; i++)  {   for(int j = 0; j < 2; j++) f[i][j] = res[i][j]; }   }
+int fib(int n)  {       if(n == 0) return 0;        if(n == 1) return 1;    
+    int f[2][2] = {{1, 1}, {1, 0}}; int res[2][2] = {{1, 0}, {0, 1}};       
+    while(n)    {   if(n & 1) multiply(res, f); multiply(f, f); n >>= 1;    }   return res[0][1] % MOD; }   
+int GCD[MX], TOTI[MX];  
+void gcdSum()  {   for(int i = 0; i < MX; i++) TOTI[i] = i;   
+    for(int i = 2; i < MX; i++) {   if(TOTI[i] == i)   {   TOTI[i] = i - 1; for(int j = 2 * i; j < MX; j += i)  {   TOTI[j] -= (TOTI[j] / i); }   }   }   
+    for(int i = 1; i < MX; i++) {   for(int j = i, k = 1; j < MX; j += i, k++)  {   GCD[j] += i * TOTI[k];   }   }
+}
+struct custom {
+    static const uint64_t C = 0x9e3779b97f4a7c15; const uint32_t RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+    size_t operator()(uint64_t x) const { return __builtin_bswap64((x ^ RANDOM) * C); }
+    size_t operator()(const std::string& s) const { size_t hash = std::hash<std::string>{}(s); return hash ^ RANDOM; } };
+template <class K, class V> using umap = std::unordered_map<K, V, custom>; template <class K> using uset = std::unordered_set<K, custom>;
+    
+
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        umap<string, int> mp;
+        for(auto& it : words) mp[it]++;
+        int len = words[0].size();
+        vi res;
+        for(int offSet = 0; offSet < len; offSet++)
+        {
+            umap<string, int> seen;
+            for(int cnt = 0, i = offSet; i <= s.size(); i += len)
+            {
+                auto f = s.substr(i, len);
+                if(!mp.count(f))
+                {
+                    seen = {};
+                    cnt = 0;
+                    continue;
+                }
+                cnt++;
+                seen[f]++;
+                while(seen[f] > mp[f])
+                {
+                    auto F = s.substr(i - (cnt - 1) * len, len);
+                    seen[F]--;
+                    cnt--;
+                }
+                if(cnt == words.size())
+                {
+                    res.pb(i - (cnt - 1) * len);
+                }
+            }
+        }
+        return res;
+    }
+};
