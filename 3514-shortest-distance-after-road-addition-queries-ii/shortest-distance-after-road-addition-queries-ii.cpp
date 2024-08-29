56b5a9a490b4d3d8a47570fcb3f27db233e5f319
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-28 20:52:36 -0700

    Time: 182 ms (97.72%) | Memory: 118.3 MB (95.78%) - LeetSync

--- 3514-shortest-distance-after-road-addition-queries-ii/shortest-distance-after-road-addition-queries-ii.cpp
+++ 3514-shortest-distance-after-road-addition-queries-ii/shortest-distance-after-road-addition-queries-ii.cpp
@@ -1,24 +1,40 @@
+class DSU { 
+    public: 
+    int n;  
+    vector<int> root;
+    DSU(int n) {    
+        this->n = n;    
+        root.resize(n, -1);
+    }
+    
+    int find(int x) {   
+        if(root[x] == -1) return x; 
+        return root[x] = find(root[x]);
+    }
+    
+    void merge(int x, int y) {  
+        x = find(x), y = find(y);   
+        if(x != y) root[x] = y, n--;
+    }
+    
+    int next(int x) {return find(x);}
+    
+    int get() { return n; }
+};
 class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        set<int> arr;
-        for (int i = 0; i < n; ++i) {
-            arr.insert(i);
+        DSU root(n);
+        vector<int> res;
+        vector<bool> vis(n);
+        for(auto& q : queries) {
+            int start = q[0], end = q[1] - 1;
+            while(start < end) {
+                root.merge(start, start + 1);
+                start = root.next(start);
+            }
+            res.push_back(root.get() - 1);
         }
-        
-        vector<int> result;
-        for (const auto& query : queries) {
-            int i = query[0];
-            int j = query[1];
-            
-            // merge all edges between [i, j]
-            // == remove all nodes [i + 1, i + 2 ... j - 1]
-            auto it1 = arr.lower_bound(i + 1);
-            auto it2 = arr.upper_bound(j - 1);
-            
-            arr.erase(it1, it2);
-            result.push_back(arr.size() - 1);
-        }
-        return result;
+        return res;
     }
 };
