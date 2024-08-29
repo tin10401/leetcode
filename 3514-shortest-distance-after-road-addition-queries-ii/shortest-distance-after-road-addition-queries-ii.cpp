94bfd82b6cfc17e30dbfd8e58a149119b751b197
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-28 21:02:07 -0700

    Time: 171 ms (99.02%) | Memory: 113.3 MB (97.26%) - LeetSync

--- 3514-shortest-distance-after-road-addition-queries-ii/shortest-distance-after-road-addition-queries-ii.cpp
+++ 3514-shortest-distance-after-road-addition-queries-ii/shortest-distance-after-road-addition-queries-ii.cpp
@@ -1,40 +1,14 @@
-class DSU { 
-    public: 
-    int n;  
-    vector<int> root;
-    DSU(int n) {    
-        this->n = n;    
-        root.resize(n, -1);
-    }
-    
-    int find(int x) {   
-        if(root[x] == -1) return x; 
-        return root[x] = find(root[x]);
-    }
-    
-    void merge(int x, int y) {  
-        x = find(x), y = find(y);   
-        if(x != y) root[x] = y, n--;
-    }
-    
-    int next(int x) {return find(x);}
-    
-    int get() { return n; }
-};
 class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        DSU root(n);
-        vector<int> res;
-        vector<bool> vis(n);
-        for(auto& q : queries) {
-            int start = q[0], end = q[1] - 1;
-            while(start < end) {
-                root.merge(start, start + 1);
-                start = root.next(start);
-            }
-            res.push_back(root.get() - 1);
+        vector<int> ans, jump(--n); 
+        iota(jump.begin(), jump.end(), 1); 
+        for (auto& q : queries) {
+            int u = q[0], v = q[1]; 
+            for (; jump[u] < v; --n) 
+                tie(jump[u], u, u) = make_tuple(v, jump[u], jump[u]);
+            ans.push_back(n); 
         }
-        return res;
+        return ans; 
     }
 };
