e46e6de217e6286304c9d95db1e06c2f84babac5
tin10401 <101690568+tin10401@users.noreply.github.com>
2024-08-31 21:57:56 -0700

    Time: 85 ms (29.41%) | Memory: 23.4 MB (82.35%) - LeetSync

--- 3563-select-cells-in-grid-with-maximum-score/select-cells-in-grid-with-maximum-score.cpp
+++ 3563-select-cells-in-grid-with-maximum-score/select-cells-in-grid-with-maximum-score.cpp
@@ -1,3 +1,4 @@
+int dp[1 << 11][12][12];
 class Solution {
 public:
     int maxScore(vector<vector<int>>& grid) {
@@ -5,13 +6,16 @@ public:
         for(int i = 0; i < n; i++) {
             sort(rbegin(grid[i]), rend(grid[i]));
         } 
-        auto dfs = [&](auto& dfs, int mask = 0, int last = 101) -> int {
-            int best = 0;
+        memset(dp, -1, sizeof(dp)); 
+        auto dfs = [&](auto& dfs, int mask = 0, int row = 11, int col = 11) -> int {
+            int& best = dp[mask][row][col];
+            if(best != -1) return best;
+            best = 0;
             for(int i = 0; i < n; i++) {
                 if((mask >> i) & 1) continue;
                 for(int j = 0; j < m; j++) {
-                    if(grid[i][j] < last) {
-                        best = max(best, grid[i][j] + dfs(dfs, mask | (1 << i), grid[i][j]));
+                    if(row == 11 || grid[i][j] < grid[row][col]) {
+                        best = max(best, grid[i][j] + dfs(dfs, mask | (1 << i), i, j));
                         break;
                     }
                 }
